ab0622efbbeb591d246e98147ba6c1e53e18934e
Rickey07 <Rickeyrickey747@gmail.com>
2023-06-10 17:15:47 +0530

    Feat - Basic Todo List Completed

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,67 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
+type typeOfTask = {
+  id: string;
+  title: string;
+  completed: boolean;
+  createdAt: string;
+};
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const listContainer = document.querySelector<HTMLUListElement>(
+  '#todo-list-container',
+);
+const form = document.querySelector<HTMLFormElement>('#main-form');
+const input = document.querySelector<HTMLInputElement>('#inputEl');
+
+const tasks: typeOfTask[] = loadTasks();
+
+tasks.forEach((task) => {
+  addTaskItem(task)
+})
+
+form?.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (input?.value === '' || input === null) return;
+
+  const task: typeOfTask = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date().toDateString(),
+  };
+  tasks.push(task);
+  addTaskItem(task);
+});
+
+function addTaskItem(task: typeOfTask) {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.addEventListener('change', () => {
+    task.completed = checkbox.checked;
+    if (checkbox.checked) {
+      item.style.color = 'red';
+    } else {
+      item.style.color = 'black';
+    }
+  });
+  task.completed = checkbox.checked;
+  label.append(checkbox, task.title);
+  item.append(label);
+  listContainer?.append(item);
+  saveTasks()
+}
+
+function saveTasks():void {
+  localStorage.setItem("Tasks",JSON.stringify(tasks))
+}
+
+function loadTasks():typeOfTask[] {
+  const tasks = localStorage.getItem("Tasks")
+  if(tasks === null) {
+    return []
+  }
+  return JSON.parse(tasks)
+}
